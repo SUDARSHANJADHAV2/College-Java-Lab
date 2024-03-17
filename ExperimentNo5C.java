6f4fa1bbd8092ef4eb1ccb3f520a43a693ac88ba
SUDARSHAN SANTAJI JADHAV <145355498+SUDARSHANJADHAV2@users.noreply.github.com>
2024-03-17 13:25:30 +0530

    Update ExperimentNo5C.java

--- ExperimentNo5C.java
+++ ExperimentNo5C.java
@@ -5,7 +5,6 @@ abstract class Shape {
 class Rectangle extends Shape {
     private double length;
     private double width;
-
     public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
