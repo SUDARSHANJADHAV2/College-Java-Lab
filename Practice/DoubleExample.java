d90d7fa965f6aa1cafda0e0aac2eebece5f008cc
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-10 22:58:35 +0530

    Create DoubleExample.java

---
+++ Practice/DoubleExample.java
@@ -0,0 +1,9 @@
+//Purpose: Holds a 64-bit floating-point number.
+public class DoubleExample
+{
+    public static void main(String[] args){
+        double pi=3.14159;
+        
+        System.out.println("Value of pi : "+pi);
+    }
+}
