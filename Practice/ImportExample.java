3e8444a0a34a5b9556e184c67d7f8f7886c3ef95
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-10 22:59:04 +0530

    Create ImportExample.java

---
+++ Practice/ImportExample.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class ImportExample {
+    public static void main(String[] args){
+        ArrayList<String> names=new ArrayList<>();
+        names.add("Sudarshan");
+        names.add("Ujal");
+        System.out.println("Names : "+ names);
+    }
+}
