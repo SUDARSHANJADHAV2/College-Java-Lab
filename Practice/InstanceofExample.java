814c535a580d37187f391b53e4a30acaffad1b17
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-10 22:59:12 +0530

    Create InstanceofExample.java

---
+++ Practice/InstanceofExample.java
@@ -0,0 +1,9 @@
+public class InstanceofExample {
+    public static void main(String[] args){
+        String text="Hello, World!";    
+        
+        if(text instanceof String){
+            System.out.println("the object is a String.");
+        }
+    }
+}
