b3933431ff4d53f33ec9791a60cf13df284bce56
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-10 22:58:37 +0530

    Create ElseExample.java

---
+++ Practice/ElseExample.java
@@ -0,0 +1,15 @@
+//Purpose: Specifies the alternate path in an if statement.
+
+public class ElseExample 
+{
+    public static void main(String[] args)
+    {
+        int number =10;
+        if(number>15){
+            System.out.println("Number is greater than 15");
+        }
+        else{
+            System.out.println("Number is less than or equal to 15");
+        }
+    }
+}
