256cdeaf5b0fda28acf36aa33a3400ee3d22c481
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-10 22:59:27 +0530

    Create InvertedHalfPyramidWithNo.java

---
+++ Practice/InvertedHalfPyramidWithNo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class InvertedHalfPyramidWithNo {
+    public static void main(String[] args) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i + j <= n) {
+                    System.out.print(j);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
