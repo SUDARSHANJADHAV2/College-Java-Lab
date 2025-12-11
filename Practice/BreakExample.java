eceb52960ac2e1a48e6c96b1f39168182edba421
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-11 12:21:03 +0530

    Create BreakExample.java

---
+++ Practice/BreakExample.java
@@ -0,0 +1,13 @@
+//Purpose: Breaks out of a loop or switch case.
+public class BreakExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                System.out.println("Breaking the loop at i=" + i);
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
