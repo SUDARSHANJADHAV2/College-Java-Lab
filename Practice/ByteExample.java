154401125eb95dabe0489531639949c8ae3add74
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-11 12:20:58 +0530

    Create ByteExample.java

---
+++ Practice/ByteExample.java
@@ -0,0 +1,7 @@
+//Purpose: Represents an 8-bit signed integer.
+public class ByteExample {
+    public static void main(String[] args) {
+        byte smallNumber = 127; // value ranges : -128 to 127
+        System.out.println("Byte Value : " + smallNumber);
+    }
+}
