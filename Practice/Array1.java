d4c6c71e870e85ca2483596a534912886a8d7043
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-11 12:20:34 +0530

    Create Array1.java

---
+++ Practice/Array1.java
@@ -0,0 +1,14 @@
+public class Array1 {
+    public static void main(String args[]) {
+        int[] array;
+        array = new int[5];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element at index " + i + " : " + array[i]);
+        }
+    }
+}
