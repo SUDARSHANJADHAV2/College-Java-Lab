37e7e8befa66982985fac450cd276c45fbabb10e
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-12 12:21:42 +0530

    Create ClassExample.java

---
+++ Practice/ClassExample.java
@@ -0,0 +1,19 @@
+//Purpose: Declares a new class.
+import java.util.*;
+
+class MyClass{
+    int number;
+    MyClass(int num)
+    {
+        this.number=num;
+    }
+    void displayNumber(){
+        System.out.println("The number is : "+number);
+    }
+}
+
+public class ClassExample {
+    public static void main(String[] args){
+
+    }
+}
