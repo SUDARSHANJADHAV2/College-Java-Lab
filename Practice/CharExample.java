02355d924c6f8bc84ac9e925520d0d6a2a8585ab
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-12 12:21:39 +0530

    Create CharExample.java

---
+++ Practice/CharExample.java
@@ -0,0 +1,8 @@
+//Purpose: Holds a single 16-bit Unicode character.
+public class CharExample {
+    public static void main(String[] args) {
+        char letter = 'A';
+
+        System.out.println("Character" + letter);
+    }
+}
