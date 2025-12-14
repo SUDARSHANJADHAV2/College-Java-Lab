bf75c13ce98a9436d4241154c022e3930e50c7bc
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-14 22:56:40 +0530

    Create HelloSwitch.java

---
+++ Practice/HelloSwitch.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class HelloSwitch {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt(); 
+        switch (button) {
+            case 1: {
+                System.out.println("hello");
+                break;
+            }
+            
+            case 2: {
+                System.out.println("Namaste");
+                break;
+            }
+            case 3: {
+                System.out.println("Ram Ram");
+                break;
+            }
+            default: {
+                System.out.println("invalid input");
+            }
+        }
+    }
+}
