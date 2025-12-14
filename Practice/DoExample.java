f70fa51f4e73314c4d817df2cca17c0313c03d20
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-14 22:55:33 +0530

    Create DoExample.java

---
+++ Practice/DoExample.java
@@ -0,0 +1,12 @@
+//Purpose: Starts a do-while loop that executes the block at least once.
+public class DoExample
+ {
+    public static void main(String[] args){
+        int i=1;
+        do{
+            System.out.println(" Value of i: "+i);
+            i++;
+        }
+        while(i<=3);
+    }
+}
