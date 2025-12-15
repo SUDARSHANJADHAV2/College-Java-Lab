0fdc6ce114cea70e6216c9800c340492d4e11689
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-15 22:54:49 +0530

    Create DeleteElement.java

---
+++ Practice/DeleteElement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class DeleteElement {
+    public static void main(String args){
+        int array[]=new int[4];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Elements : ");
+        for(int i = 0 ; i < array.length;i++){
+            array[i]=sc.nextInt();
+        }
+        for(int i = 0 ; i < array.length;i++){
+            if(array[i]==2){
+                
+        }
+
+        }
+    }
+}
