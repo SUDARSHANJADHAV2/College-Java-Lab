2fef73a64a3622f444a229536c14033937af93da
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-12-15 22:54:00 +0530

    Create BooleanExample.java

---
+++ Practice/BooleanExample.java
@@ -0,0 +1,12 @@
+
+//Purpose: Represents true or false values.
+import java.util.*;
+
+public class BooleanExample {
+    public static void main(String args[]) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println("Is Java fun?" + isJavaFun);
+        System.out.println("Is fish tasty?" + isFishTasty);
+    }
+}
